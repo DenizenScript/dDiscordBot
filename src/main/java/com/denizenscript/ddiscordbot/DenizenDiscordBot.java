dde65a348e7e8308f94225a17565cc9de2576a53
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-07-23 06:41:16 -0700

    use new tag base registration format

--- src/main/java/com/denizenscript/ddiscordbot/DenizenDiscordBot.java
+++ src/main/java/com/denizenscript/ddiscordbot/DenizenDiscordBot.java
@@ -5,13 +5,8 @@ import com.denizenscript.ddiscordbot.objects.*;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import com.denizenscript.denizencore.DenizenCore;
 import com.denizenscript.denizencore.events.ScriptEvent;
-import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.ObjectFetcher;
-import com.denizenscript.denizencore.tags.TagRunnable;
-import com.denizenscript.denizencore.tags.Attribute;
-import com.denizenscript.denizencore.tags.ReplaceableTagEvent;
 import com.denizenscript.denizencore.tags.TagManager;
-import com.denizenscript.denizencore.utilities.CoreUtilities;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -40,71 +35,36 @@ public class DenizenDiscordBot extends JavaPlugin {
             ObjectFetcher.registerWithObjectFetcher(DiscordGroupTag.class, DiscordGroupTag.tagProcessor);
             ObjectFetcher.registerWithObjectFetcher(DiscordRoleTag.class, DiscordRoleTag.tagProcessor);
             ObjectFetcher.registerWithObjectFetcher(DiscordUserTag.class, DiscordUserTag.tagProcessor);
-            TagManager.registerTagHandler(new TagRunnable.RootForm() {
-                @Override
-                public void run(ReplaceableTagEvent event) {
-                    discordTagBase(event);
+            // <--[tag]
+            // @attribute <discord[<bot-id>]>
+            // @returns DiscordBotTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns the Discord bot for the given bot ID.
+            // -->
+            TagManager.registerTagHandler("discord", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    attribute.echoError("Discord tag base must have input.");
+                    return null;
                 }
-            }, "discord");
-            TagManager.registerTagHandler(new TagRunnable.RootForm() {
-                @Override
-                public void run(ReplaceableTagEvent event) {
-                    discordEmbedTagBase(event);
+                return DiscordBotTag.valueOf(attribute.getContext(1), attribute.context);
+            });
+            // <--[tag]
+            // @attribute <discord_embed>
+            // @returns DiscordEmbedTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns a blank DiscordEmbedTag instance, to be filled with data via the with.as tag.
+            // -->
+            TagManager.registerTagHandler("discord_embed", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    return new DiscordEmbedTag();
                 }
-            }, "discord_embed");
+                return DiscordEmbedTag.valueOf(attribute.getContext(1), attribute.context);
+            });
         }
         catch (Throwable ex) {
             Debug.echoError(ex);
         }
-    }
-
-    public void discordEmbedTagBase(ReplaceableTagEvent event) {
-        if (!event.matches("discord_embed") || event.replaced()) {
-            return;
-        }
-        DiscordEmbedTag embed;
-        if (event.hasNameContext()) {
-            embed = DiscordEmbedTag.valueOf(event.getNameContext(), event.getAttributes().context);
-        }
-        else {
-            embed = new DiscordEmbedTag();
-        }
-        Attribute attribute = event.getAttributes().fulfill(1);
-        event.setReplacedObject(CoreUtilities.autoAttrib(embed, attribute));
-    }
-
-    public void discordTagBase(ReplaceableTagEvent event) {
-        if (!event.matches("discord") || event.replaced()) {
-            return;
-        }
-        DiscordBotTag bot = null;
-        if (event.hasNameContext()) {
-            bot = DiscordBotTag.valueOf(event.getNameContext(), event.getAttributes().context);
-        }
-        Attribute attribute = event.getAttributes().fulfill(1);
-
-        // <--[tag]
-        // @attribute <discord[<bot-id>].exists>
-        // @returns ElementTag(Boolean)
-        // @plugin dDiscordBot
-        // @description
-        // Returns whether a Discord bot exists with the given bot ID.
-        // -->
-        if (attribute.startsWith("exists")) {
-            event.setReplacedObject(CoreUtilities.autoAttrib(new ElementTag(bot != null), attribute.fulfill(1)));
-            return;
-        }
-
-        // <--[tag]
-        // @attribute <discord[<bot-id>]>
-        // @returns DiscordBotTag
-        // @plugin dDiscordBot
-        // @description
-        // Returns the Discord bot for the given bot ID.
-        // -->
-        if (bot == null) {
-            return;
-        }
-        event.setReplacedObject(CoreUtilities.autoAttrib(bot, attribute));
     }
 }
