926afad204c6b325743a0f812845443ba410f6aa
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-07-23 22:09:10 -0700

    enable intents for members and messages

--- src/main/java/com/denizenscript/ddiscordbot/DiscordCommand.java
+++ src/main/java/com/denizenscript/ddiscordbot/DiscordCommand.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.bukkit.Bukkit;
 
@@ -205,7 +206,7 @@ public class DiscordCommand extends AbstractCommand implements Holdable {
         @Override
         public void run() {
             try {
-                JDA jda = JDABuilder.createDefault(code).setMemberCachePolicy(MemberCachePolicy.ALL).build();
+                JDA jda = JDABuilder.createDefault(code).enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES).setMemberCachePolicy(MemberCachePolicy.ALL).build();
                 conn.client = jda;
                 jda.awaitReady();
                 conn.registerHandlers();
