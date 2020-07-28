591556304ac9b7937bca0a619a3c83c8563a963e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-07-28 11:34:43 -0700

    add base tags for dDiscordBot object types

--- src/main/java/com/denizenscript/ddiscordbot/DenizenDiscordBot.java
+++ src/main/java/com/denizenscript/ddiscordbot/DenizenDiscordBot.java
@@ -50,17 +50,79 @@ public class DenizenDiscordBot extends JavaPlugin {
                 return DiscordBotTag.valueOf(attribute.getContext(1), attribute.context);
             });
             // <--[tag]
-            // @attribute <discord_embed>
+            // @attribute <discord_channel[<channel>]>
+            // @returns DiscordChannelTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns a Discord Channel object constructed from the input value.
+            // Refer to <@link language DiscordChannelTag objects>.
+            // -->
+            TagManager.registerTagHandler("discord_channel", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    attribute.echoError("Discord channel tag base must have input.");
+                    return null;
+                }
+                return DiscordChannelTag.valueOf(attribute.getContext(1), attribute.context);
+            });
+            // <--[tag]
+            // @attribute <discord_embed[(<embed>)>
             // @returns DiscordEmbedTag
             // @plugin dDiscordBot
             // @description
             // Returns a blank DiscordEmbedTag instance, to be filled with data via the with.as tag.
+            // Or, if given an input, returns a Discord Embed object constructed from the input value.
+            // Refer to <@link language DiscordEmbedTag objects>.
             // -->
             TagManager.registerTagHandler("discord_embed", (attribute) -> {
                 if (!attribute.hasContext(1)) {
                     return new DiscordEmbedTag();
                 }
                 return DiscordEmbedTag.valueOf(attribute.getContext(1), attribute.context);
+            });
+            // <--[tag]
+            // @attribute <discord_group[<group>]>
+            // @returns DiscordGroupTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns a Discord Group object constructed from the input value.
+            // Refer to <@link language DiscordGroupTag objects>.
+            // -->
+            TagManager.registerTagHandler("discord_group", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    attribute.echoError("Discord group tag base must have input.");
+                    return null;
+                }
+                return DiscordGroupTag.valueOf(attribute.getContext(1), attribute.context);
+            });
+            // <--[tag]
+            // @attribute <discord_role[<role>]>
+            // @returns DiscordRoleTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns a Discord Role object constructed from the input value.
+            // Refer to <@link language DiscordRoleTag objects>.
+            // -->
+            TagManager.registerTagHandler("discord_role", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    attribute.echoError("Discord role tag base must have input.");
+                    return null;
+                }
+                return DiscordRoleTag.valueOf(attribute.getContext(1), attribute.context);
+            });
+            // <--[tag]
+            // @attribute <discord_user[<user>]>
+            // @returns DiscordUserTag
+            // @plugin dDiscordBot
+            // @description
+            // Returns a Discord User object constructed from the input value.
+            // Refer to <@link language DiscordUserTag objects>.
+            // -->
+            TagManager.registerTagHandler("discord_user", (attribute) -> {
+                if (!attribute.hasContext(1)) {
+                    attribute.echoError("Discord user tag base must have input.");
+                    return null;
+                }
+                return DiscordUserTag.valueOf(attribute.getContext(1), attribute.context);
             });
         }
         catch (Throwable ex) {
