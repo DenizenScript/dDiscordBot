5beff8991e34d70b7290263af04e1ac28015631e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-10-18 10:58:04 -0700

    use new getRawValue

--- src/main/java/com/denizenscript/ddiscordbot/DiscordCommand.java
+++ src/main/java/com/denizenscript/ddiscordbot/DiscordCommand.java
@@ -181,7 +181,7 @@ public class DiscordCommand extends AbstractCommand implements Holdable {
                 scriptEntry.addObject("message_id", arg.asElement());
             }
             else if (!scriptEntry.hasObject("message")) {
-                scriptEntry.addObject("message", new ElementTag(arg.raw_value));
+                scriptEntry.addObject("message", new ElementTag(arg.getRawValue()));
             }
             else {
                 arg.reportUnhandled();
