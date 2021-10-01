4b1d3bc658a4ea7572a3359f117cebdeabe8c638
acikek <spiralixp@gmail.com>
2021-10-01 15:01:18 -0700

    interactions pt 2 (2)

---
+++ src/main/java/com/denizenscript/ddiscordbot/events/DiscordSelectionUsedScriptEvent.java
@@ -0,0 +1,95 @@
+package com.denizenscript.ddiscordbot.events;
+
+import com.denizenscript.ddiscordbot.DiscordScriptEvent;
+import com.denizenscript.ddiscordbot.objects.DiscordChannelTag;
+import com.denizenscript.ddiscordbot.objects.DiscordGroupTag;
+import com.denizenscript.ddiscordbot.objects.DiscordInteractionTag;
+import com.denizenscript.ddiscordbot.objects.DiscordSelectionTag;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
+
+public class DiscordSelectionUsedScriptEvent extends DiscordScriptEvent {
+
+    public static DiscordSelectionUsedScriptEvent instance;
+
+    // <--[event]
+    // @Events
+    // discord selection used
+    //
+    // @Regex ^on discord selection used$
+    //
+    // @Switch for:<bot> to only process the event for a specified Discord bot.
+    // @Switch channel:<channel_id> to only process the event when it occurs in a specified Discord channel.
+    // @Switch group:<group_id> to only process the event for a specified Discord group.
+    // @Switch id:<menu_id> to only process the event for a specified Discord selection menu.
+    //
+    // @Triggers when a Discord user uses a selection menu.
+    //
+    // @Plugin dDiscordBot
+    //
+    // @Group Discord
+    //
+    // @Context
+    // <context.bot> returns the relevant Discord bot object.
+    // <context.channel> returns the channel.
+    // <context.group> returns the group.
+    // <context.interaction> returns the interaction.
+    // <context.menu> returns the selection menu.
+    // <context.options> returns the selected options.
+    //
+    // -->
+
+    public SelectionMenuEvent getEvent() {
+        return (SelectionMenuEvent) event;
+    }
+
+    @Override
+    public boolean couldMatch(ScriptPath path) {
+        return path.eventLower.startsWith("discord selection used");
+    }
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!path.checkSwitch("channel", getEvent().getChannel().getId())) {
+            return false;
+        }
+        if (path.switches.containsKey("group")) {
+            if (!getEvent().isFromGuild()) {
+                return false;
+            }
+            if (!path.checkSwitch("group", getEvent().getGuild().getId())) {
+                return false;
+            }
+        }
+        if (!path.checkSwitch("id", getEvent().getSelectionMenu().getId())) {
+            return false;
+        }
+        return super.matches(path);
+    }
+
+    @Override
+    public ObjectTag getContext(String name) {
+        switch (name) {
+            case "channel":
+                return new DiscordChannelTag(botID, getEvent().getChannel());
+            case "group":
+                if (getEvent().isFromGuild()) {
+                    return new DiscordGroupTag(botID, getEvent().getGuild());
+                }
+                break;
+            case "interaction":
+                return DiscordInteractionTag.getOrCreate(botID, getEvent().getInteraction());
+            case "menu":
+                return new DiscordSelectionTag(getEvent().getSelectionMenu());
+            case "options":
+                return DiscordSelectionTag.getSelectionOptions(getEvent().getSelectedOptions());
+        }
+
+        return super.getContext(name);
+    }
+
+    @Override
+    public String getName() {
+        return "DiscordSelectionUsed";
+    }
+}
---
+++ src/main/java/com/denizenscript/ddiscordbot/objects/DiscordSelectionTag.java
@@ -0,0 +1,283 @@
+package com.denizenscript.ddiscordbot.objects;
+
+import com.denizenscript.denizencore.objects.Fetchable;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.MapTag;
+import com.denizenscript.denizencore.tags.Attribute;
+import com.denizenscript.denizencore.tags.ObjectTagProcessor;
+import com.denizenscript.denizencore.tags.TagContext;
+import com.denizenscript.denizencore.tags.TagRunnable;
+import com.denizenscript.denizencore.utilities.CoreUtilities;
+import com.denizenscript.denizencore.utilities.text.StringHolder;
+import net.dv8tion.jda.api.entities.Emoji;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.SelectionMenu;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class DiscordSelectionTag implements ObjectTag {
+
+    // <--[ObjectType]
+    // @name DiscordSelectionTag
+    // @prefix discordselection
+    // @base ElementTag
+    // @format
+    // The identity format for Discord selection menu is a map of menu data. Do not alter raw menu data, use the with.as tag instead.
+    //
+    // @plugin dDiscordBot
+    // @description
+    // A DiscordSelectionTag is an object that represents a Discord selection menu for use with dDiscordBot.
+    //
+    // -->
+
+    @Fetchable("discordselection")
+    public static DiscordSelectionTag valueOf(String string, TagContext context) {
+        if (string.startsWith("discordselection@")) {
+            string = string.substring("discordselection@".length());
+        }
+        MapTag map = MapTag.valueOf(string, context);
+        if (map == null) {
+            return null;
+        }
+        return new DiscordSelectionTag(map);
+    }
+
+    @Override
+    public DiscordSelectionTag duplicate() {
+        return new DiscordSelectionTag(menuData.duplicate());
+    }
+
+    public static boolean matches(String arg) {
+        if (arg.startsWith("discordselection@")) {
+            return true;
+        }
+        return MapTag.matches(arg);
+    }
+
+    public DiscordSelectionTag() {
+        menuData = new MapTag();
+    }
+
+    public DiscordSelectionTag(MapTag map) {
+        menuData = map;
+    }
+
+    public static MapTag getSelectionOptions(List<SelectOption> selectOptions) {
+        MapTag options = new MapTag();
+        if (selectOptions != null) {
+            for (SelectOption option : selectOptions) {
+                options.putObject("label", new ElementTag(option.getLabel()));
+                options.putObject("value", new ElementTag(option.getValue()));
+                if (option.getDescription() != null) {
+                    options.putObject("description", new ElementTag(option.getDescription()));
+                }
+                if (option.getEmoji() != null) {
+                    options.putObject("emoji", new ElementTag(option.getEmoji().getName()));
+                }
+            }
+        }
+        return options;
+    }
+
+    public DiscordSelectionTag(SelectionMenu menu) {
+        menuData = new MapTag();
+        if (menu.getId() != null) {
+            menuData.putObject("id", new ElementTag(menu.getId()));
+        }
+        menuData.putObject("options", DiscordSelectionTag.getSelectionOptions(menu.getOptions()));
+    }
+
+    public SelectionMenu.Builder build(TagContext context) {
+        ObjectTag id = menuData.getObject("id");
+        MapTag options = (MapTag) menuData.getObject("options");
+        SelectionMenu.Builder menu = null;
+        if (id != null) {
+            menu = SelectionMenu.create(id.toString());
+        }
+        else {
+            return null;
+        }
+        if (options != null) {
+            for (ObjectTag optionObj : options.map.values()) {
+                MapTag option = optionObj.asType(MapTag.class, context);
+                ElementTag label = (ElementTag) option.getObject("label");
+                ElementTag value = (ElementTag) option.getObject("value");
+                ElementTag description = (ElementTag) option.getObject("description");
+                ElementTag emoji = (ElementTag) option.getObject("emoji");
+                Emoji emojiData = null;
+                if (emoji != null) {
+                    emojiData = Emoji.fromUnicode(emoji.toString());
+                }
+                if (label == null || value == null) {
+                    return null;
+                }
+                if (description == null && emoji == null) {
+                    menu.addOption(label.toString(), value.toString());
+                }
+                else if (emoji == null) {
+                    menu.addOption(label.toString(), value.toString(), description.toString());
+                }
+                else if (description == null) {
+                    if (emojiData != null) {
+                        menu.addOption(label.toString(), value.toString(), emojiData);
+                    }
+                }
+                else {
+                    if (emojiData != null) {
+                        menu.addOption(label.toString(), value.toString(), description.toString(), emojiData);
+                    }
+                }
+            }
+        }
+        return menu;
+    }
+
+    public MapTag menuData;
+
+    public static HashSet<String> acceptedWithKeys = new HashSet<>(Arrays.asList(
+        "id", "options"
+    ));
+
+    public static void registerTags() {
+
+        // <--[tag]
+        // @attribute <DiscordSelectionTag.with_map[<map>]>
+        // @returns DiscordSelectionTag
+        // @plugin dDiscordBot
+        // @description
+        // Returns a copy of this Button tag, with the map of keys to values applied.
+        // Refer to <@link tag DiscordSelectionTag.with.as>.
+        // -->
+        registerTag("with_map", (attribute, object) -> {
+            DiscordSelectionTag menu = object.duplicate();
+            if (!attribute.hasContext(1)) {
+                attribute.echoError("Invalid selection.with_map[...] tag: must have an input value.");
+                return null;
+            }
+            MapTag map = MapTag.getMapFor(attribute.getContextObject(1), attribute.context);
+            for (Map.Entry<StringHolder, ObjectTag> entry : map.map.entrySet()) {
+                String key = entry.getKey().low;
+                if (!acceptedWithKeys.contains(key)) {
+                    attribute.echoError("Invalid selection.with_map[...] tag: unknown key '" + key + "' given.");
+                    return null;
+                }
+                ObjectTag val = entry.getValue();
+                if (val == null) {
+                    attribute.echoError("selection.with_map[...] value is invalid.");
+                    return null;
+                }
+                menu.menuData.putObject(key, val);
+            }
+            return menu;
+        });
+
+        // <--[tag]
+        // @attribute <DiscordSelectionTag.with[<key>].as[<value>]>
+        // @returns DiscordSelectionTag
+        // @plugin dDiscordBot
+        // @description
+        // Returns a copy of this Selection tag, with the specified data key set to the specified value.
+        // The following keys are accepted, with values of the listed type:
+        // id: ElementTag
+        // options: MapTag where the values are also a MapTag consisting of:
+        // - label: ElementTag
+        // - value: ElementTag
+        // - description: ElementTag
+        // - emoji: ElementTag
+        // -->
+        registerTag("with", (attribute, object) -> {
+            DiscordSelectionTag menu = object.duplicate();
+            if (!attribute.hasContext(1)) {
+                attribute.echoError("Invalid selection.with[...] tag: must have an input value.");
+                return null;
+            }
+            String key = CoreUtilities.toLowerCase(attribute.getContext(1));
+            if (!acceptedWithKeys.contains(key)) {
+                attribute.echoError("Invalid selection.with[...] tag: unknown key '" + key + "' given.");
+                return null;
+            }
+            attribute.fulfill(1);
+            if (!attribute.startsWith("as") || !attribute.hasContext(1)) {
+                attribute.echoError("selection.with[...] must be followed by as[...].");
+            }
+            ObjectTag val = attribute.getContextObject(1);
+            if (val == null) {
+                attribute.echoError("selection.with[...].as[...] value is invalid.");
+                return null;
+            }
+            menu.menuData.putObject(key, val);
+            return menu;
+        });
+
+        // <--[tag]
+        // @attribute <DiscordSelectionTag.map>
+        // @returns MapTag
+        // @plugin dDiscordBot
+        // @description
+        // Returns the MapTag internally backing this selection tag.
+        // -->
+        registerTag("map", (attribute, object) -> {
+            return object.menuData.duplicate();
+        });
+    }
+
+    public static ObjectTagProcessor<DiscordSelectionTag> tagProcessor = new ObjectTagProcessor<>();
+
+    public static void registerTag(String name, TagRunnable.ObjectInterface<DiscordSelectionTag> runnable, String... variants) {
+        tagProcessor.registerTag(name, runnable, variants);
+    }
+
+    @Override
+    public ObjectTag getObjectAttribute(Attribute attribute) {
+        return tagProcessor.getObjectAttribute(this, attribute);
+    }
+
+    String prefix = "discordselection";
+
+    @Override
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @Override
+    public String debuggable() {
+        return "discordselection@" + menuData.debuggable();
+    }
+
+    @Override
+    public boolean isUnique() {
+        return false;
+    }
+
+    @Override
+    public String getObjectType() {
+        return "DiscordSelection";
+    }
+
+    @Override
+    public String identify() {
+        return "discordselection@" + menuData.identify();
+    }
+
+    @Override
+    public String identifySimple() {
+        return identify();
+    }
+
+    @Override
+    public String toString() {
+        return identify();
+    }
+
+    @Override
+    public ObjectTag setPrefix(String prefix) {
+        if (prefix != null) {
+            this.prefix = prefix;
+        }
+        return this;
+    }
+}
